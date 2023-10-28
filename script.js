3e5ab6f2caa81e17deb34b37b0f83e70e2a4f1e6
BDunnCode <BDunnCode@github.com>
2023-10-28 14:01:43 -0500

    update time loop. mostly fixing edge cases

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ const worldElem = document.querySelector('[data-world]')
 setPixelToWorldScale()
 window.addEventListener("resize", setPixelToWorldScale)
 
+let lastTime
 function update(time) {
+  if (lastTime == null) {
+    lastTime = time
+    window.requestAnimationFrame(update)
+    return
+  }
+    const delta = time - lastTime
+    console.log(time)
 
+    lastTime = time
     window.requestAnimationFrame(update)
 }
 window.requestAnimationFrame(update)
